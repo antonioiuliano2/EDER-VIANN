42c617b1505acc912de40dd9ad79e04f2ed6c8df
FilipsFedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2021-06-17 20:48:48 +0100

    Delete Code directory

--- Code/Utilities/dummy.py
+++
@@ -1,5 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser(description='select cut parameters')
-parser.add_argument('--f',help="Please enter the input file location", default='n/a')
-args = parser.parse_args()
-print(args.f)
